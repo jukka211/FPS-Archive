dc759c590a12efaab55581ab1493bbc33693e0d9
jukka211 <158295066+jukka211@users.noreply.github.com>
2024-06-03 18:26:02 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,95 @@
+function changeText(id) {
+  document.getElementById("changeable-text").innerHTML = id + "FPS";
+}
+
+function restoreText() {
+  document.getElementById("changeable-text").innerHTML = "23FPS";
+}
+
+function triggerTextChange(id) {
+  changeText(id);
+}
+
+
+
+document.addEventListener('DOMContentLoaded', function () {
+  const cursorLine = document.getElementById('cursor-line');
+
+  document.addEventListener('mousemove', function (event) {
+      const mouseX = event.clientX;
+      cursorLine.style.left = mouseX + 'px';
+  });
+});
+
+
+
+$(document).ready(function() {
+  $('.column').click(function() {
+     $(this).css('mix-blend-mode', 'normal');
+     
+     
+     
+  });
+});
+
+
+$(document).ready(function() {
+  $('#01, #02, #03, #04, #05, #06, #07, #08, #09, #10, #11, #12, #13, #14, #15, #16, #17, #18, #19, #20, #21, #22, #23').click(function() {
+     var screenHeight = $(window).height(); // Get the height of the window
+     var flexValue = Math.round(screenHeight / 1.59); // Calculate the flex value based on the screen height
+     $(this).css('flex', flexValue);
+  });
+});
+
+
+
+
+
+
+
+
+//Sonst einfach löschen
+$(document).ready(function() {
+  // Check if the device is a desktop
+  if(!(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent))) {
+    // Code to execute only on desktops
+    $('#01, #02, #03, #04, #05, #06, #07, #08, #09, #10, #11, #12, #13, #14, #15, #16, #17, #18, #19, #20, #21, #22, #23').hover(function() {
+      var flexValue;
+      if ($(window).height() > 1500) {
+        flexValue = '3';
+        $('#01, #02, #03, #04, #05, #06, #07, #08, #09, #10, #11, #12, #13, #14, #15, #16, #17, #18, #19, #20, #21, #22, #23').not(this).css('flex', '1');
+      } else if ($(window).height() > 1000) {
+        flexValue = '3';
+        $('#01, #02, #03, #04, #05, #06, #07, #08, #09, #10, #11, #12, #13, #14, #15, #16, #17, #18, #19, #20, #21, #22, #23').not(this).css('flex', '1');
+      } else {
+        flexValue = '3';
+        $('#01, #02, #03, #04, #05, #06, #07, #08, #09, #10, #11, #12, #13, #14, #15, #16, #17, #18, #19, #20, #21, #22, #23').not(this).css('flex', '1');
+      }
+      $(this).css('flex', flexValue);
+    });
+  }
+});
+
+
+
+document.addEventListener("DOMContentLoaded", function() {
+  // Select all images within the .column class
+  var images = document.querySelectorAll('.column img');
+
+  // Function to change visibility of images with fadeIn animation after a delay
+  function showImages() {
+    images.forEach(function(img, index) {
+      // Add fadeIn animation to each image
+      img.style.visibility = 'visible';
+     
+    });
+  }
+
+  // Set a timeout to call the showImages function after a 500ms delay
+  setTimeout(showImages, 100);
+});
+
+
+
+ 
+
